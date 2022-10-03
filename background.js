20468366f2ff87ab9de1aaef0c997b6785eb5918
KushagraAgarwal525 <kushagraagarwal525.ka@gmail.com>
2022-10-03 21:05:49 +0530

    Buggy code

--- background.js
+++ background.js
@@ -1,12 +1,39 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "addHighlight",
+    title: "Highlight",
+    contexts: ["selection"],
   });
-  
+
+  chrome.contextMenus.create({
+    id: "addNote",
+    title: "Add Note",
+    contexts: ["selection"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+  const text = item.selectionText;
+  if (item.menuItemId === "addHighlight") {
+    chrome.tabs.sendMessage(tab.id, {
+      type: "ADD_HIGHLIGHT",
+      text,
+    });
+    console.log(text);
+  } else if (item.menuItemId === "addNote") {
+    chrome.tabs.sendMessage(tab.id, {
+      type: "ADD_NOTE",
+      text,
+    });
+    console.log(text);
+  }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url) {
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      webUrl: tab.url,
+    });
+  }
+});
--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,33 @@
 (() => {
-    let youtubeLeftControls, youtubePlayer;
-    let currentVideo = "";
-    let currentVideoBookmarks = [];
+  console.log("in content script");
+  let youtubeLeftControls, youtubePlayer;
+  let currentWebsite = "";
+  let currentVideoBookmarks = [];
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
-
-        if (type === "NEW") {
-            currentVideo = videoId;
-            newVideoLoaded();
-        }
-    });
-
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
-
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
-
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
-
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
-            youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
-        }
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    if (obj.type === "NEW") {
+      currentWebsite = webUrl;
+      console.log("neww!!!!!!!!!!!");
+      newWebsiteOpened();
+    } else if (type === "ADD_HIGHLIGHT") {
+      console.log("highlight added");
     }
+  });
 
-    const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
+  const newWebsiteOpened = () => {
+    return "";
+  };
 
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
-
-    newVideoLoaded();
 })();
 
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
+// Source: https://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
 
-    return date.toISOString().substr(11, 0);
+function getSelectionText() {
+  let text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+  return text;
 }
